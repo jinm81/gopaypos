884230a37275012d63e13da3ea676d810864b239
hojin Mun <jin1281@gmail.com>
2020-11-03 21:20:21 -0500

    navbar

---
+++ gopaypos/src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Nav, NavbarContainer} from './Navbar.elements';
+
+const Navbar = () => {
+    return (
+        <>
+        <nav>
+           <Navbarcontainer>
+
+           </Navbarcontainer>
+        </nav>
+    </>
+    );
+};
+
+export default Navbar
